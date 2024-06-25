03f6db3da9950db72bb6a7b9ba80108f19c4bb87
root <root@Pasztecik>
2024-06-25 19:41:04 +0200

    [kolejne]

---
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div>
+        <button @click="pobierzWpisy">Refresh</button>
+        siema blog
+
+        {{ wpisy }}
+    </div>
+
+</template>
+
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+
+    export default{
+        data(){
+            return{
+                wpisy: []
+            }
+        },
+        methods: {
+            async pobierzWpisy(){
+            this.wpisy =  await dzien2_backend.odczytaj_wpisy();
+            },
+        }
+    }
+
+</script>
