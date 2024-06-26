89703ad0da815e7dec5342b935bfde9616f4dcfc
root <root@Pasztecik>
2024-06-26 19:11:49 +0200

    style + poprawki

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,48 +1,41 @@
 <template>
-
     <div>
         <h2 class="text-blue-600">Wpisy na bloga:</h2>
         <div class="w-100 flex flex-row-reverse">
-            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4" >refresh</button>
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
         </div>
         <div class="grid mx-6 gap-4 my-4">
-            <div v-for="wpis in wpisy"
-            class ="drop-shadow-x1 bg-stone-300 p-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
                 <p>{{ wpis }}</p>
             </div>
         </div>
         <div class="flex justify-center flex-col">
-            <input v-model="nowyBlog" class="border-2 border-blue-600
-            p-4" type="text">
-            <button class="bg-blue-600 rounded text-white p-4" @click="dodajWpis">dodaj</button>
+            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+            <button  class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
         </div>
     </div>
-
 </template>
 
 <script>
 import { dzien2_backend } from 'declarations/dzien2_backend/index';
-import { onMounted } from 'vue';
 
-    export default{
-        data(){
-            return{
-                wpisy: [],
-                nowyBlog: " "
-            }
-        },
-        methods: {
-            async dodajWpisy(){
-            await dzien2_backend.dodaj_wpis(this.nowyBlog);
-            },
-            async pobierzWpisy(){
-            this.wpisy =  await dzien2_backend.odczytaj_wpisy();
-            },
-            async mounted() {
-            this.pobierzWpisy();
-            }
+export default {
+    data() {
+        return {
+            wpisy: [],
+            nowyBlog: ""
         }
-            
+    },
+    methods: {
+        async dodajWpisy() {
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy() {
+            this.wpisy = await dzien2_backend.odczytaj_wpisy();
+        }
+    },
+    async mounted(){
+        this.pobierzWpisy()
     }
-
+}
 </script>
